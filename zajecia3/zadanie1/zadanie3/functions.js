dc826e6b31cf884ff46772a3d01fbab6ef6721a4
nhermann <161588164+nadiahermann111@users.noreply.github.com>
2024-11-21 10:18:15 +0100

    .

---
+++ zajecia3/zadanie1/zadanie3/functions.js
@@ -0,0 +1,11 @@
+function range(a, b) {
+    const result = [];
+    for (let i = a; i <= b; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  
+  console.log(range(3, 8)); 
+  console.log(range(1, 5)); 
+  

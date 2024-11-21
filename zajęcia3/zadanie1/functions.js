481a79a2916ef50fcaad69e44394df04e3c562f6
nhermann <161588164+nadiahermann111@users.noreply.github.com>
2024-11-21 09:03:01 +0100

    .

--- zajęcia3/zadanie1/functions.js
+++ zajęcia3/zadanie1/functions.js
@@ -6,4 +6,6 @@ const isEven = (num) => num % 2 === 0;
 
 
 console.log("Czy liczba 1 jest nieparzysta?", isOdd(1));
+console.log("Czy liczba 10 jest nieparzysta?", isOdd(10));
 console.log("Czy liczba 8 jest parzysta?", isEven(8));
+console.log("Czy liczba 3 jest parzysta?", isEven(3));
